d367e6ceeee79361a4d3955ee19f08fcf92412ae
chengzh2008 <chengzh2008@gmail.com>
2020-10-21 21:49:22 -0700

    initial setup and up to 1.1.7

---
+++ README.md
@@ -0,0 +1,13 @@
+# Structure and Interpretation of Computer Program in Rust
+
+## edit and compile
+
+'''
+cargo watch
+'''
+
+## edit and test
+
+'''
+cargo watch -x test
+'''
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod build_abstraction_with_function;
+
+use crate::build_abstraction_with_function::compound_function;
+
+fn main() {
+    let _result = compound_function::hello();
+}
